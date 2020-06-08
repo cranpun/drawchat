c6fac441f0b03116178705275b93c95ff89f0ce7
cranpun <cranpun@gmail.com>
2020-06-08 20:27:54 +0900

    レイアウトの調整

--- resources/views/paper.blade.php
+++ resources/views/paper.blade.php
@@ -1,97 +1,90 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" style="transform-origin: top left;">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="initial-scale=1">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" style="transform-origin: top left; overflow-x: scroll;">
 
-        <title>{{ config("app.name") }}</title>
+<head>
+    @include("element/head")
+    <title>{{ config("app.name") }}</title>
+    <script src="{{ asset('js/app.js') . '?v=' . filemtime(public_path() .'/js/app.js') }}" defer></script>
+</head>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-        <link rel="shortcut icon" href="https://tm.cranpun-tool.ml/wp-content/themes/themeorg/favicon.ico" />
+<body style="padding: 10px;">
+    <header style="position: fixed; z-index: 999;">
+        <nav class="navbar is-light" role="navigation" aria-label="main navigation">
+            <section class="navbar-brand">
+                <h2 class="navbar-item" data-testid="paper-{{ $paper_id }}">
+                    paper {{ $paper_id }}
+                </h2>
+                <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false"
+                    data-target="navbarPaper">
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                </a>
+            </section>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                margin: 0;
+            <section id="navbarPaper" class="navbar-menu">
+                <div class="navbar-start">
+                    <a id="bt-save" class="navbar-item">
+                        保存
+                    </a>
+
+                    <a class="navbar-item" href="/">
+                        戻る
+                    </a>
+
+                    <div class="navbar-item has-dropdown is-hoverable">
+                        <a class="navbar-link">
+                            More
+                        </a>
+
+                        <div class="navbar-dropdown">
+                            <a class="navbar-item">
+                                About
+                            </a>
+                            <a class="navbar-item">
+                                Jobs
+                            </a>
+                            <a class="navbar-item">
+                                Contact
+                            </a>
+                            <hr class="navbar-divider">
+                            <a class="navbar-item">
+                                Report an issue
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </nav>
+    </header>
+    <main style="padding-top: 55px;">
+        <style type="text/css">
+            <?php $cw=2200; $ch=1600; ?>
+            #drawcanvases {
+                border: 3px solid #aaa;
+                border-radius: 5px;
             }
 
-            .content {
-                text-align: center;
+            #drawcanvases,
+            #mycanvas,
+            #othercanvas {
+                width: <?=$cw?>px;
+                height: <?=$ch?>px;
             }
 
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
+            #mycanvas,
+            #othercanvas {
+                position: absolute;
+                top: 0;
+                left: 0;
             }
         </style>
-        <script src="{{ asset('js/app.js') . '?v=' . filemtime(public_path() .'/js/app.js') }}" defer></script>
-    </head>
-    <body>
-        <div class="">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div>
-                <div class="title m-b-md">
-                    paper {{ $paper_id }}
-                </div>
-                <style type="text/css">
-                    <?php $cw = 2200; $ch = 1600; ?>
-                    #drawcanvases {
-                        margin-bottom: 15px;
-                        border: 3px solid #aaa;
-                        border-radius: 5px;
-                    }
-                    #drawcanvases, #mycanvas, #othercanvas {
-                        width: <?= $cw ?>px;
-                        height: <?= $ch ?>px;
-                    }
-                    #mycanvas, #othercanvas {
-                        position: absolute;
-                        top: 0;
-                        left: 0;
-                    }
-                </style>
-                <div id="drawcanvases" style="position: relative">
-                    <canvas id="othercanvas" width="<?= $cw ?>" height="<?= $ch ?>"></canvas>
-                    <canvas id="mycanvas" width="<?= $cw ?>" height="<?= $ch ?>"></canvas>
-                </div>
-
-                <div class="links">
-                    <a href="/">戻る</a>
-                    <button id="bt-save">保存</button>
-                    <button id="bt-load" style="display: none">ロード</button>
-                </div>
-            </div>
+        <div id="drawcanvases" style="position: relative">
+            <canvas id="othercanvas" width="<?=$cw?>" height="<?=$ch?>"></canvas>
+            <canvas id="mycanvas" width="<?=$cw?>" height="<?=$ch?>"></canvas>
         </div>
-    </body>
+    </main>
+    <footer></footer>
+</body>
+
 </html>
